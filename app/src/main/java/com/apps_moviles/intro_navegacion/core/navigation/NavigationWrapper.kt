83b01df6e6f32dc265c0b0e178798de93af7f3b7
JuanGallego1107 <gallego1107@gmail.com>
2025-11-07 14:56:33 -0500

    Added: Agrega screens

--- app/src/main/java/com/apps_moviles/intro_navegacion/core/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/apps_moviles/intro_navegacion/core/navigation/NavigationWrapper.kt
@@ -2,6 +2,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.apps_moviles.intro_navegacion.DetailScreen
+import com.apps_moviles.intro_navegacion.core.navigation.Detail
 import com.apps_moviles.intro_navegacion.core.navigation.Home
 import com.apps_moviles.intro_navegacion.core.navigation.Login
 
@@ -13,7 +16,12 @@ fun NavigationWrapper() {
             LoginScreen {navController.navigate(Home)}
         }
         composable<Home> {
-            HomeScreen()
+            HomeScreen{name -> navController.navigate(Detail(name = name))}
+        }
+        composable<Detail> { backStackEntry ->
+            val detail: Detail = backStackEntry.toRoute()
+            DetailScreen(name = detail.name)
+
         }
     }
 }
